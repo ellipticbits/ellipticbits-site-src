cf13a4c20f8c40c758bd5575178266878e9584b4
Mario <the4power@yahoo.com.ar>
2019-02-27 17:11:33 -0300

    translations

--- js/agency.js
+++ js/agency.js
@@ -1,6 +1,14 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $('#linkToEnglish').click(function() {
+    window.location.replace('../en/');
+  });
+
+  $('#linkToSpanish').click(function() {
+    window.location.replace('../es/');
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
